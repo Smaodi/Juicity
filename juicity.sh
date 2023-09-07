e6de9a6c12e8ea191924ae73f622cc450a37e937
Smaodi <143680588+Smaodi@users.noreply.github.com>
2023-09-07 22:24:24 +0330

    Update juicity.sh

--- juicity.sh
+++ juicity.sh
@@ -109,53 +109,9 @@ sudo systemctl restart juicity
 
 
 # Prompt user for choice
-read -p "Select an option (1 or 2): 1) Irancell--> IPV6   2) Hamrah-Aval--> IPV4 , Default (1): " choice
+# read -p "Select an option (1 or 2): 1) Irancell--> IPV6   2) Hamrah-Aval--> IPV4 , Default (1): " choice
 
-case $choice in
-    1)
-        # Get IPv6 address
-        IPv6_ADDRESS=$(ip -6 addr show dev ens3 | awk '/inet6 .*global/{print $2}' | cut -d '/' -f 1)
 
-        # Original share link
-        SHARE_LINK_IPV4=$(/root/juicity/./juicity-server generate-sharelink -c /root/juicity/config_server.json)
-
-        # Replace IPv4 with IPv6 in the share link
-        SHARE_LINK_IPV6=$(echo "$SHARE_LINK_IPV4" | sed "s/[0-9]\+\(\.[0-9]\+\)\{3\}/[$IPv6_ADDRESS]/g")
-
-        echo "----------------------------------------------------------"
-        echo ""
-        echo "Link with IPv6: -->  $SHARE_LINK_IPV6"
-        echo "----------------------------------------------------------"
-        echo "$SHARE_LINK_IPV6" > link.txt  #save
-        ;;
-    2)
-       # Original share link
-        SHARE_LINK_IPV4=$(/root/juicity/./juicity-server generate-sharelink -c /root/juicity/config_server.json)
-        echo "----------------------------------------------------------"
-        echo " "
-        echo "Link with IPv4: -->  $SHARE_LINK_IPV4"
-        echo "----------------------------------------------------------"
-        echo "$SHARE_LINK_IPV4" > link.txt  #save
-        ;;
-    *)
-        # Default choice
-        choice=1
-        # Get IPv6 address
-        IPv6_ADDRESS=$(ip -6 addr show dev ens3 | awk '/inet6 .*global/{print $2}' | cut -d '/' -f 1)
-
-        # Original share link
-        SHARE_LINK_IPV4=$(/root/juicity/./juicity-server generate-sharelink -c /root/juicity/config_server.json)
-
-        # Replace IPv4 with IPv6 in the share link
-        SHARE_LINK_IPV6=$(echo "$SHARE_LINK_IPV4" | sed "s/[0-9]\+\(\.[0-9]\+\)\{3\}/[$IPv6_ADDRESS]/g")
-
-        echo "----------------------------------------------------------"
-        echo ""
-        echo "Link with IPv6: -->  $SHARE_LINK_IPV6"
-        echo "----------------------------------------------------------"
-        echo "$SHARE_LINK_IPV6" > link.txt  #save
-        esac
-        ;;
     2)
         # Check if Juicity is installed
         if [[ -f /etc/systemd/system/juicity.service ]]; then
